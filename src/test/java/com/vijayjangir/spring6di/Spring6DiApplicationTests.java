b2146ebd7b6040396c572c484b5acc2491886168
Vijay Jangir <6284383+vijay-jangir@users.noreply.github.com>
2023-08-26 09:27:22 +0530

    tested how spring context, autowired context, and autowired controller directly in the test cases

--- src/test/java/com/vijayjangir/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/com/vijayjangir/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,30 @@
 package com.vijayjangir.spring6di;
 
+import com.vijayjangir.spring6di.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+
+	@Autowired
+	ApplicationContext applicationContext;
+
+	@Autowired
+	MyController myController;
+
+	@Test
+	void testAutowiredController() {
+		myController.sayHello();
+	}
+
+	@Test
+	void testGetControllerFromCtx() {
+		MyController myController = applicationContext.getBean(MyController.class);
+		myController.sayHello();
+	}
 
 	@Test
 	void contextLoads() {
